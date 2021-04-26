585e35207ab7c74be99053bce6a28793be97b657
naedavis <naeemahndavis@gmail.com>
2021-04-26 12:15:25 +0200

    Added main.py

--- main.py
+++ main.py
@@ -1,17 +1,29 @@
 #dictionary
 #Task1
 # number of times a character appears in a string
-# #
-# string = input('Enter string : ')
-# value = input('Enter character to be looked for : ')
-#
-# print(string.count(value))
+
+string = input('Enter string : ')
+value = input('Enter character to be looked for : ')
+
+print(string.count(value)) # counting in the string variable received from the user
+                        # the amount of value variable in the string variable
+
 
 #Task2
 #printing digits into words
-#
-number = list(input('enter a number to be converted: '))
+
+number = list(input('enter a number to be converted: ')) # convetered to a list so its easier to run through in the loop
 list_words = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
 
-for x in number:
-    print(str(list_words[int(x)])) #square brackets to say i just want this specific position in this list
+for x in number: # x in the input received from the user
+     print(str(list_words[int(x)]))
+     #converted x to an int because it was returning a string value initially
+     # because of the input variable and the postion in a list cant be  a string variable
+     # #square brackets to say i just want this specific position in this list
+
+
+
+
+
+
+
